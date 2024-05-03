6db8c6442a6f7d18ba4ef920245d1d2a79cc5305
mairess <trabalho.maires@gmail.com>
2024-05-03 07:06:59 -0300

    feat: implements method findHighConsumptionDevices on class EnergyAccount

--- src/main/java/com/betrybe/minhaconta/business/EnergyAccount.java
+++ src/main/java/com/betrybe/minhaconta/business/EnergyAccount.java
@@ -1,7 +1,10 @@
 package com.betrybe.minhaconta.business;
 
+import com.ions.lightdealer.sdk.model.Address;
 import com.ions.lightdealer.sdk.model.Client;
 import com.ions.lightdealer.sdk.model.ElectronicDevice;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -19,6 +22,21 @@ public class EnergyAccount {
    * Req. 11 – Find high consumption device per address.
    */
   public ElectronicDevice[] findHighConsumptionDevices() {
-    return new ElectronicDevice[0];
+    Address[] addresses = client.getAddressesAsArray();
+    ElectronicDevice[] highConsumptionDevices = new ElectronicDevice[addresses.length];
+
+    for (int i = 0; i < addresses.length; i += 1) {
+      Address address = addresses[i];
+      ElectronicDevice[] devices = address.getDevicesAsArray();
+      ElectronicDevice highestDevice = null;
+
+      for (ElectronicDevice device : devices) {
+        if (highestDevice == null || device.monthlyKwh() > highestDevice.monthlyKwh()) {
+          highestDevice = device;
+        }
+      }
+      highConsumptionDevices[i] = highestDevice;
+    }
+    return highConsumptionDevices;
   }
 }
